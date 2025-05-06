6f046630ba5f4049869945871ce0f6107767c2cc
Alex <163790087+AlexUeck@users.noreply.github.com>
2025-05-06 15:27:28 +1000

    imports, new scenes, credits, audio fixes

--- Scenes/Worlds/main_menu.gd
+++ Scenes/Worlds/main_menu.gd
@@ -26,3 +26,7 @@ func _on_options_pressed():
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_button_down() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Credits/Credits.tscn")
