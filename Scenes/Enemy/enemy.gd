80ca5c362197c6ea3f0339faa37139e7ddaa14ec
EmmetHughes <0257695@schoolsnet.act.edu.au>
2025-05-16 09:15:55 +1000

    can't switch weapons while reloading

--- Scenes/Enemy/enemy.gd
+++ Scenes/Enemy/enemy.gd
@@ -5,7 +5,7 @@ extends CharacterBody3D
 @onready var enemy = $enemy
 @onready var pistol = $enemymodel/Pistol # Reference the gun node (pistol) in the enemy scene
 @onready var player = null #get_node("/root/Player")  
-var SPEED = 5
+var SPEED = 2
 const JUMP_VELOCITY = 4.5
 var bullet_scene = preload("res://Scenes/Enemy/enemy_bullet.tscn")
 @export var shooting_offset: Vector3 = Vector3(0, 1, 3)  # Adjust where bullets should spawn
