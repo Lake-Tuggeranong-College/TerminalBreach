41901685f8ab57db580f26f05596532cf945b235
Nadal Rahman <nadal.rahman07@gmail.com>
2025-05-23 10:24:50 +1000

    another fixed animations

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -188,9 +188,6 @@ func _physics_process(delta):
 			player_anim_player.play("rifleidle")
 
 	move_and_slide()
-	
-
-	wwwwwww
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "shoot":
