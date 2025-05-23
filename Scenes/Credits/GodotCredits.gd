daa0a78a299a70ee9ad2a0537ba83e7f5eb8a421
Jordan-Rogers102 <159493236+Jordan-Rogers102@users.noreply.github.com>
2025-05-23 10:10:06 +1000

    enemies full map range

--- Scenes/Credits/GodotCredits.gd
+++ Scenes/Credits/GodotCredits.gd
@@ -106,72 +106,72 @@ var credits = [
 		"Web Development",
 		"Nadal"
 	],[
-		#"Web Dev",
-		#"Nadal"
-	#],[
-		#"Dev Web",
-		#"Nadal"
-	#],[
-		#"Web Spinner",
-		#"Nadal"
-	#],[
-		#"Maker of web",
-		#"Nadal"
-	#],[
-		#"Developing web",
-		#"Nadal"
-	#],[
-		#"Webologist",
-		#"Nadal"
-	#],[
-		#"Weberfile",
-		#"Nadal"
-	#],[
-		#"Front-End Development",
-		#"Nadal"
-	#],[
-		#"Back-End Development",
-		#"Nadal"
-	#],[
-		#"All-Around-End Development",
-		#"Nadal"
-	#],[
-		#"Full-Stack Development",
-		#"Nadal"
-	#],[
-		#"UI/UX Development",
-		#"Nadal"
-	#],[
-		#"Software Engineering (Web)",
-		#"Nadal"
-	#],[
-		#"Web Application Development",
-		#"Nadal"
-	#],[
-		#"Digital Development",
-		#"Nadal"
-	#],[
-		#"Credit stealer",
-		#"Nadal"
-	#],[
-		#"Building the Front-End / Back-End / All-Around-End",
-		#"Nadal"
-	#],[
-		#"Coding for the Web",
-		#"Nadal"
-	#],[
-		#"Writing Web Apps",
-		#"Nadal"
-	#],[
-		#"DevOps (for infrastructure-heavy work)",
-		#"Nadal"
-	#],[
-		#"Cloud Web Dev",
-		#"Nadal"
-	#],[
-		#"Just a guy",
-		#"Nadal"
-	#],[
+		"Web Dev",
+		"Nadal"
+	],[
+		"Dev Web",
+		"Nadal"
+	],[
+		"Web Spinner",
+		"Nadal"
+	],[
+		"Maker of web",
+		"Nadal"
+	],[
+		"Developing web",
+		"Nadal"
+	],[
+		"Webologist",
+		"Nadal"
+	],[
+		"Weberfile",
+		"Nadal"
+	],[
+		"Front-End Development",
+		"Nadal"
+	],[
+		"Back-End Development",
+		"Nadal"
+	],[
+		"All-Around-End Development",
+		"Nadal"
+	],[
+		"Full-Stack Development",
+		"Nadal"
+	],[
+		"UI/UX Development",
+		"Nadal"
+	],[
+		"Software Engineering (Web)",
+		"Nadal"
+	],[
+		"Web Application Development",
+		"Nadal"
+	],[
+		"Digital Development",
+		"Nadal"
+	],[
+		"Credit stealer",
+		"Nadal"
+	],[
+		"Building the Front-End / Back-End / All-Around-End",
+		"Nadal"
+	],[
+		"Coding for the Web",
+		"Nadal"
+	],[
+		"Writing Web Apps",
+		"Nadal"
+	],[
+		"DevOps (for infrastructure-heavy work)",
+		"Nadal"
+	],[
+		"Cloud Web Dev",
+		"Nadal"
+	],[
+		"Just a guy",
+		"Nadal"
+	],[
 		"Thanks for playing!"]
 	
 ]
