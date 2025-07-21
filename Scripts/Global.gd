e02b39b5c29be85bae5b042b129cca598edbadb8
Ryan Cather <ryan.cather@ed.act.edu.au>
2025-07-21 22:49:17 +1000

    set ip temporary

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -5,7 +5,11 @@ const PORT = 9999
 #var enet_peer = ENetMultiplayerPeer.new()
 
 var single_player_mode = true
-var address_server = null
+#var address_server = null
+var address_server :String = "192.168.68.106"
+
+	
+
 
 var checkboxChecked := false
 # if status is true, increases the size of the window and scales on-screen elements to window size
@@ -18,6 +22,7 @@ func high_resolution_display_mode(status):
 			get_window().move_to_center() # Centres the screen
 
 func _ready():
+	print("Server :",address_server)
 	high_resolution_display_mode(true)
 
 var player = null
