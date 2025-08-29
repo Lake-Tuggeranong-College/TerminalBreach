cb288f0a61cd29387d0eec32e1a2eedbcab957d5
Alex <0627778@schoolsnet.act.edu.au>
2025-08-29 10:14:38 +1000

    update code idk wat but it fixed issue

--- node_3d.gd
+++ node_3d.gd
@@ -6,9 +6,6 @@ var player: Node3D = null
 
 func _ready():
 	await get_tree().create_timer(0.1).timeout
-	while player == null:
-		player = get_tree().get_root().find_child("Player", true, false)
-		await get_tree().create_timer(0.1).timeout
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Player"):
